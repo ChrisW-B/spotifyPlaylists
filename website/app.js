5e8f56279a4242378b48b914edf4dab5e1b630a2
Christopher Barry <me@chrisb.xyz>
2016-09-14 11:49:48 -0400

    No more handling ssl in nodejs

--- website/app.js
+++ website/app.js
@@ -8,25 +8,6 @@ var SpotifyWebApi = require('spotify-web-api-node'),
 	scribe = require('scribe-js')(),
 	console = process.console,
 	app = express();
-var lex = require('letsencrypt-express').create({
-	// set to https://acme-v01.api.letsencrypt.org/directory in production
-	server: 'https://acme-v01.api.letsencrypt.org/directory',
-	challenges: {
-		'http-01': require('le-challenge-fs').create({
-			webrootPath: '/tmp/acme-challenges'
-		})
-	},
-	store: require('le-store-certbot').create({
-		webrootPath: '/tmp/acme-challenges'
-	}),
-	approveDomains: ['spotifyapps.chriswbarry.com'],
-	email: 'me@chriswbarry.com',
-	agreeTos: true,
-	debug: true
-});
-require('http').createServer(lex.middleware(require('redirect-https')())).listen(5620, function() {
-	console.log("Listening for ACME http-01 challenges on", this.address());
-});
 config.recentlyAdded.spotifyApi = new SpotifyWebApi({
 	clientId: config.recentlyAdded.clientId,
 	clientSecret: config.recentlyAdded.clientSecret,
@@ -272,8 +253,8 @@ app.get('/recentlyadded/thanks', function(req, res) {
 		type: 'recently added'
 	});
 });
-require('https').createServer(lex.httpsOptions, lex.middleware(app)).listen(5621, function() {
-	console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
+app.listen(5621, function() {
+	console.log('SpotifyApps listening on port 5621!');
 });
 
 function getCreds(type, unsub) {
