929e7d3ec9ea2753dccb3f55adaaecef8bbb30dc
Christopher Barry <me@chrisb.xyz>
2016-10-25 10:05:02 -0400

    Left some test in by accident

--- app.js
+++ app.js
@@ -291,5 +291,5 @@ setInterval(recentlyAdded.start, 5 * ONE_HOUR);
 setTimeout(() => setInterval(mostPlayed.start, 5 * ONE_HOUR), 2 * ONE_HOUR); //offset start
 
 //run after start
-mostPlayed.start();
+recentlyAdded.start();
 setTimeout(mostPlayed.start, 3 * ONE_MIN);
